008b44b41835423872fd051c5ba30de6f543c144
Nihal00 <83398133+Nihal00@users.noreply.github.com>
2023-05-13 17:04:53 +0530

    Update script.js
    F2 Contest-1 May, JavaScript solution

--- script.js
+++ script.js
@@ -8,20 +8,52 @@ let arr = [
 
 function PrintStudentswithMap() {
   //Write your code here , just console.log
+  arr.map(students => {
+    if(students.marks > 50){
+      console.log(students);
+    }
+  });
 }
 
 function PrintStudentsbyForEach() {
   //Write your code here , just console.log
+  arr.forEach(students => {
+    if(students.marks > 50){
+      console.log(students);
+    }
+  });
 }
 
 function addData() {
   //Write your code here, just console.log
+  let newStudent = {
+    id: 4,
+    name: "Susan",
+    age: "20",
+    marks: 45
+  }
+  arr.push(newStudent);
+  console.log(arr);
 }
 
 function removeFailedStudent() {
   //Write your code here, just console.log
+  let failedStudentIndex = arr.findIndex(students => students.marks < 50);
+
+  arr.splice(failedStudentIndex, 1);
+
+  console.log(arr);
 }
 
 function concatenateArray() {
   //Write your code here, just console.log
+  let newArray = [
+    { id: 5, name: "Rajinikanth", age: "21", marks: 100 },
+    { id: 6, name: "Amitabh", age: "25", marks: 89 },
+    { id: 7, name: "kiran", age: "27", marks: 49 },
+  ]
+
+  let concatenatedArr = arr.concat(newArray);
+
+  console.log(concatenatedArr);
 }
